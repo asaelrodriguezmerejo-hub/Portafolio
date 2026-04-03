a59dc638ac47ee4042d3a8186c5467c46a687c25
asaelrodriguezmerejo-hub <asaelrodriguezmerejo@gmail.com>
2026-04-02 23:59:16 -0400

    Add files via upload

---
+++ Practica/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.btn-pagina');
+button.addEventListener('click', () => {
+    window.location.href = 'https://www.google.com';
+});
+
+const contactButton = document.querySelector('.btn-redes');
+contactButton.addEventListener('click', () => {
+    window.location.href = 'https://www.linkedin.com';
+});
